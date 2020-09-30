c6f7a7fdba8e22106d4f04f571ee586fdd0fe6e0
mariaeloisaveras <63668790+mariaeloisaveras@users.noreply.github.com>
2020-09-30 17:58:20 -0300

    Add files via upload

---
+++ data/Equipamentossociais_11.js
@@ -0,0 +1,19 @@
+var json_Equipamentossociais_11 = {
+"type": "FeatureCollection",
+"name": "Equipamentossociais_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nome": "Conselho Tutelar I" }, "geometry": { "type": "Point", "coordinates": [ -46.888385523568346, -23.190616932878203 ] } },
+{ "type": "Feature", "properties": { "nome": "Conselho Tutelar II" }, "geometry": { "type": "Point", "coordinates": [ -46.879668624927632, -23.198453480697708 ] } },
+{ "type": "Feature", "properties": { "nome": "Conselho Tutelar III" }, "geometry": { "type": "Point", "coordinates": [ -46.962706671679157, -23.190035668567337 ] } },
+{ "type": "Feature", "properties": { "nome": "CRAS_CENTRAL" }, "geometry": { "type": "Point", "coordinates": [ -46.887561949077458, -23.187909736160233 ] } },
+{ "type": "Feature", "properties": { "nome": "CRAS_NOVO_HORIZONTE" }, "geometry": { "type": "Point", "coordinates": [ -46.981175294235186, -23.144141724066749 ] } },
+{ "type": "Feature", "properties": { "nome": "CRAS_Sao_Camilo" }, "geometry": { "type": "Point", "coordinates": [ -46.866045702424856, -23.178616372271687 ] } },
+{ "type": "Feature", "properties": { "nome": "CRAS_STA_GERTRUDES" }, "geometry": { "type": "Point", "coordinates": [ -46.851779580915597, -23.243637531059683 ] } },
+{ "type": "Feature", "properties": { "nome": "CRAS_TAMOIO" }, "geometry": { "type": "Point", "coordinates": [ -46.853319174443001, -23.188688706518626 ] } },
+{ "type": "Feature", "properties": { "nome": "CRAS_VISTA_ALEGRE" }, "geometry": { "type": "Point", "coordinates": [ -46.921488124851699, -23.131531002719132 ] } },
+{ "type": "Feature", "properties": { "nome": "CREAS" }, "geometry": { "type": "Point", "coordinates": [ -46.889613368620594, -23.181477900527753 ] } },
+{ "type": "Feature", "properties": { "nome": "CRIJU" }, "geometry": { "type": "Point", "coordinates": [ -46.878014603125806, -23.193121123020166 ] } },
+{ "type": "Feature", "properties": { "nome": "Vila Dignidade" }, "geometry": { "type": "Point", "coordinates": [ -46.849812203915739, -23.185319252256132 ] } }
+]
+}
