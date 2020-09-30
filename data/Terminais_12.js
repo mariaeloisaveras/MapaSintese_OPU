eb60dfe9d075f0272936d71477e239a0ad41ff79
mariaeloisaveras <63668790+mariaeloisaveras@users.noreply.github.com>
2020-09-30 18:13:48 -0300

    Add files via upload

---
+++ data/Terminais_12.js
@@ -0,0 +1,15 @@
+var json_Terminais_12 = {
+"type": "FeatureCollection",
+"name": "Terminais_12",
+"features": [
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal  Eloy Chaves", "cod": "A" }, "geometry": { "type": "Point", "coordinates": [ 298696.812844632717315, 7434605.508134223520756 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Hortol�ndia", "cod": "D" }, "geometry": { "type": "Point", "coordinates": [ 304823.339118797390256, 7436869.992794433608651 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Central", "cod": "G" }, "geometry": { "type": "Point", "coordinates": [ 306739.012217050301842, 7434214.83918483927846 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Vila Arens", "cod": "E" }, "geometry": { "type": "Point", "coordinates": [ 308170.18, 7433799.54 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Col�nia", "cod": "B" }, "geometry": { "type": "Point", "coordinates": [ 310148.994046715088189, 7434887.347876287065446 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal CECAP", "cod": "C" }, "geometry": { "type": "Point", "coordinates": [ 303926.195781451126095, 7439244.701908219605684 ] } },
+{ "type": "Feature", "properties": { "nome": "CPTM - Esta��o Ferrovi�ria", "cod": "I" }, "geometry": { "type": "Point", "coordinates": [ 308363.078849509416614, 7433639.997730741277337 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Vila Rami", "cod": "F" }, "geometry": { "type": "Point", "coordinates": [ 307612.436368183873128, 7431224.826475769281387 ] } },
+{ "type": "Feature", "properties": { "nome": "Rodovi�ria", "cod": "H" }, "geometry": { "type": "Point", "coordinates": [ 306013.48416818556143, 7432244.751482924446464 ] } }
+]
+}
---
+++ data/ViasJundiai_15.js
@@ -0,0 +1,8 @@
+var json_ViasJundiai_15 = {
+"type": "FeatureCollection",
+"name": "ViasJundiai_15",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
