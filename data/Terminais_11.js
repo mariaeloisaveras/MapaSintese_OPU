e6fb8be3e40627ccb7fa2b23135a91c1247a6eec
mariaeloisaveras <63668790+mariaeloisaveras@users.noreply.github.com>
2020-10-01 11:07:51 -0300

    Add files via upload

---
+++ data/Terminais_11.js
@@ -0,0 +1,16 @@
+var json_Terminais_11 = {
+"type": "FeatureCollection",
+"name": "Terminais_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal  Eloy Chaves", "cod": "A" }, "geometry": { "type": "Point", "coordinates": [ -46.966714522952643, -23.185311198492052 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Hortol�ndia", "cod": "D" }, "geometry": { "type": "Point", "coordinates": [ -46.90659520871597, -23.165603337253309 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Central", "cod": "G" }, "geometry": { "type": "Point", "coordinates": [ -46.888226141405625, -23.18980028241236 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Vila Arens", "cod": "E" }, "geometry": { "type": "Point", "coordinates": [ -46.874301218095184, -23.193716878833232 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Col�nia", "cod": "B" }, "geometry": { "type": "Point", "coordinates": [ -46.854839322859632, -23.184124702294348 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal CECAP", "cod": "C" }, "geometry": { "type": "Point", "coordinates": [ -46.915050367867416, -23.144057676886238 ] } },
+{ "type": "Feature", "properties": { "nome": "CPTM - Esta��o Ferrovi�ria", "cod": "I" }, "geometry": { "type": "Point", "coordinates": [ -46.87243731341141, -23.195179721279732 ] } },
+{ "type": "Feature", "properties": { "nome": "SITU - Terminal Vila Rami", "cod": "F" }, "geometry": { "type": "Point", "coordinates": [ -46.880073710952729, -23.216897241503339 ] } },
+{ "type": "Feature", "properties": { "nome": "Rodovi�ria", "cod": "H" }, "geometry": { "type": "Point", "coordinates": [ -46.895562644712861, -23.207501538758567 ] } }
+]
+}
